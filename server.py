2ab0f40d119e4c0acf3cba141a30e81ffa787d32
timLaptop <tim.falkenhahn@gmail.com>
2019-09-29 22:28:45 +0300

    init conf for the server.py

--- server.py
+++ server.py
@@ -1,11 +1,26 @@
 import socket
+import select
 
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serverSocket.connect((socket.gethostname(), 7777))
-serverSocket.listen(13)
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+PORT = 7777
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SOL_REUSEADDR, 1)
+
+server_socket.bind((IP, PORT))
+
+server_socket.listen()
+
+sockets_list = [server_socket]
+
+clients = {}
+'''
+server_socket.listen(13)
 
 while True:
     clientSocket, address = serverSocket.accept()
     print(f"Connection from {address} has bin established")
 
     clientSocket.send(bytes("Connection established", "utf-8"))
+'''
